9553cc79ffef62c3bcb57982270fedeaac73b79a
Brian Kelly <polymonic@gmail.com>
2023-06-08 08:23:51 -0500

    Adds dedicated  Format, Tools and Libraries pages. (#36)

---
+++ app/controllers/libraries_controller.rb
@@ -0,0 +1,4 @@
+class LibrariesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+
   resources :songs
+
+  get "format", to: "format#index"
+  get "libraries", to: "libraries#index"
+  get "tools", to: "tools#index"
 end
